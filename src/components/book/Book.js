73f6f039a618f92003551b8a4504af55db398a81
Saeqa Sultani <saeqasultani@gmail.com>
2022-10-08 21:47:30 +0430

    connect to API

--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../../redux/books/books';
-import './Book.css';
 
 const Book = (props) => {
   const dispatch = useDispatch();
